047782f49204112d1c0531148c6ca6166146dbf8
Dan Michael O. Heggø <danmichaelo@gmail.com>
2013-09-18 14:05:36 +0000

    Add series

--- app/models/Document.php
+++ app/models/Document.php
@@ -17,7 +17,7 @@ class Document extends Eloquent {
      * @var array
      */
     public static $fields = array('bibsys_objektid', 'bibsys_dokid', 'bibsys_knyttid',
-        'isbn', 'title', 'subtitle', 'authors', 'year', 'cover', 'publisher', 'dewey');
+        'series', 'isbn', 'title', 'subtitle', 'authors', 'year', 'cover', 'publisher', 'dewey');
 
     /**
      * Hard validation rules for humans with brains.
